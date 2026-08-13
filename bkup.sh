14872fe35b53bc790032bed375b7d6d9bfbd998f
Salvador404 <yasermhmddoost@gmail.com>
2026-08-13 23:06:14 +0330

    Handling the possibility of running the script multiple times and causing error because of this

--- bkup.sh
+++ bkup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 str_date=$(date "+%D"|tr '/' '-')
+ls /tmp/HomeDirectory/"${str_date}.tar.gz" 2> /dev/null
+flag=$?
+if [ $flag!=0 ] ; then
+    exit 0
+fi 
 mkdir -p /tmp/HomeDirectory
 tar -czf "${str_date}.tar.gz" "$HOME"
-mv ${str_date}.tar.gz /tmp/HomeDirectory
-rm -f ./"${str_date}.tar.gz"
+mv ./${str_date}.tar.gz /tmp/HomeDirectory
+
 
 
